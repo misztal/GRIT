e706ed03169d1efe0624fd12ddbe508e8f532d0f
marek <marek.misztal@gmail.com>
2018-04-23 18:15:36 +0200

    Mininum necessary functionality for making tutorial_vortex_in_a_box is provided with python wrapping

--- GRIT/bin/tutorial_vortex_in_a_box.py
+++ GRIT/bin/tutorial_vortex_in_a_box.py
@@ -1,8 +1,62 @@
-import pygrit
+import pygrit as pg
+import math
+
+print('*** VORTEX IN A BOX ***\n')
 
 cfg_filename = '../bin/tutorial_vortex_in_a_box.cfg'
 
-config_file  = pygrit.ConfigFile()
-config_file.load(cfg_filename)
+settings  = pg.ConfigFile()
+settings.load(cfg_filename)
 
-print('*** VORTEX IN A BOX ***\n')
+parameters = pg.make_parameters_from_config_file( settings )
+
+txt_filename = settings.get_value('txt_filename','circle_enright.txt')
+output_path  = settings.get_value('output_path' ,''                  )
+max_steps    = int(settings.get_value('steps','100'))
+
+print('Loading mesh file: '+txt_filename)
+
+engine = pg.Engine2D()
+
+pg.init_engine_with_mesh_file(
+                              pg.get_data_file_path(txt_filename)
+                              , parameters
+                              , engine
+                              )
+
+def write_svg_files( frame_number ):
+    filename = output_path+pg.generate_filename('/tutorial_vortex_in_a_box',frame_number,'svg')
+    pg.svg_draw(filename, engine, parameters)
+    print('\t\twrite_svg_files() Done writting  '+filename)
+
+def do_simulation_step( ):
+    object_label = int(settings.get_value('object_label','1'))
+    phase        = pg.make_phase(engine, object_label)
+    px           = pg.VectorDouble()
+    py           = pg.VectorDouble()
+
+    pg.get_sub_range_current(engine, phase, px, py)
+
+    N  = px.size()
+    dt = 0.01
+    
+    px_new       = pg.VectorDouble(px)
+    py_new       = pg.VectorDouble(py)
+    
+    for i in range(0,N):
+        x = px[i]
+        y = py[i]
+        u =  2.0 * math.cos( math.pi * y ) * math.sin( math.pi * y ) * math.sin( math.pi*x ) * math.sin( math.pi*x )
+        v = -2.0 * math.cos( math.pi * x ) * math.sin( math.pi * x ) * math.sin( math.pi*y ) * math.sin( math.pi*y )
+        px_new[i] = px[i] + u*dt;
+        py_new[i] = py[i] + v*dt;
+
+    pg.set_sub_range_target(engine, phase, px_new, py_new)
+
+write_svg_files(0)
+
+for i in range(1, max_steps):
+    do_simulation_step()
+    engine.update(parameters)
+    write_svg_files(i)
+
--- GRIT/wrappers/python/python.cpp
+++ GRIT/wrappers/python/python.cpp
@@ -1,22 +1,37 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
 
 #include <grit.h>
 #include <glue.h>
 #include <util.h>
 
-namespace py = pybind11;
+#include <memory>
 
+//--- This is needed to use STL containers in Python
+PYBIND11_MAKE_OPAQUE(std::vector<double>);
+
+namespace py = pybind11;
+  
 PYBIND11_MODULE( pygrit, m ) 
 {
-  //using grit::param_type;
-  //using grit::engine2d_type;
+  using grit::param_type;
+  using grit::engine2d_type;
 
   using util::ConfigFile;
 
+  using glue::Phase;
+
+  //--- Binding STL containers
+
+  py::bind_vector<std::vector<double>>(m, "VectorDouble")
+    .def("size"  , &std::vector<double>::size);
+
+  //--- Binding classes from util, grit and glue
 
   py::class_<ConfigFile>(m, "ConfigFile")
     .def(py::init<>())
-  //.def("get_values", &ConfigFile::get_values)
+    //.def("get_values", &ConfigFile::get_values)
     .def("get_value" , (std::string (ConfigFile::*)( std::string const &) const) &ConfigFile::get_value)
     .def("get_value" , (std::string (ConfigFile::*)( std::string const &
                                                    , std::string const &) const) &ConfigFile::get_value)
@@ -35,9 +50,43 @@ PYBIND11_MODULE( pygrit, m )
   //                   Not sure if they'll be needed in the first release (as we should stick
   //                   to reading the parameters from the cfg files), but it should be rather
   //                   simple to add them later.
-  /*py::class_<param_type>(m, "Parameters")
+  py::class_<param_type>(m, "Parameters")
     .def(py::init([]() { return new param_type(); }) )
-    .def("get_upper_threshold", &param_type::get_upper_threshold)
-    .def("get_lower_threshold", &param_type::get_lower_threshold);*/
-}
+    //.def("number_of_subdomains", (unsigned int const & (param_type::*)() const) &param_type::number_of_subdomains)
+    //.def("number_of_subdomains", (unsigned int & (param_type::*)()) &param_type::number_of_subdomains)
+    //.def("get_upper_threshold", &param_type::get_upper_threshold)
+    //.def("get_lower_threshold", &param_type::get_lower_threshold);
 
+  py::class_<engine2d_type>(m, "Engine2D")
+    .def(py::init<>())
+    .def("update", &engine2d_type::update);
+
+  py::class_<Phase>(m, "Phase")
+    .def(py::init<>());
+
+  //--- Binding functions from util, grit and glue
+
+  m.def("get_data_file_path"              , &util::get_data_file_path                                   );
+  m.def("generate_filename"               , &util::generate_filename                                    );
+  
+  m.def("make_parameters_from_config_file", &grit::make_parameters_from_config_file                     );
+  m.def("init_engine_with_mesh_file"      , &grit::init_engine_with_mesh_file<grit::default_grit_types> );
+
+  m.def("svg_draw"                        , &glue::svg_draw 
+                                          , py::arg("filename")
+                                          , py::arg("engine")
+                                          , py::arg("parameters")
+                                          , py::arg("attribute_name_x") = "current"
+                                          , py::arg("attribute_name_y") = "current"                     );
+
+  m.def("make_phase"                      , (Phase (*)(engine2d_type const &, unsigned int const &)) &glue::make_phase );
+  m.def("get_sub_range_current"           , &glue::get_sub_range_current                                );
+  m.def("set_sub_range_target"            , &glue::set_sub_range_target                                 
+                                          , py::arg("engine")
+                                          , py::arg("phase")
+                                          , py::arg("x")
+                                          , py::arg("y")
+                                          , py::arg("using_partial_data") = false                       );
+
+
+}
