df83ad0a53a19b80ba25af040c7417fbe0d9cd03
marek <marek.misztal@gmail.com>
2018-04-23 12:59:40 +0200

    Wrapped util::ConfigFile, moving the output to bin

---
+++ GRIT/bin/tutorial_vortex_in_a_box.py
@@ -0,0 +1,8 @@
+import pygrit
+
+cfg_filename = '../bin/tutorial_vortex_in_a_box.cfg'
+
+config_file  = pygrit.ConfigFile()
+config_file.load(cfg_filename)
+
+print('*** VORTEX IN A BOX ***\n')
--- GRIT/wrappers/python/python.cpp
+++ GRIT/wrappers/python/python.cpp
@@ -1,36 +1,43 @@
 #include <pybind11/pybind11.h>
 
-struct Pet
-{
-  std::string m_name;
-
-  Pet()
-    : m_name("no name")
-  { }
-
-  Pet(std::string const & name)
-    : m_name(name)
-  { }
-
-  void set_name(std::string const & value)
-  {
-    m_name = value;
-  }
-
-  std::string const & get_name() const
-  {
-    return m_name;
-  }
-
-};
+#include <grit.h>
+#include <glue.h>
+#include <util.h>
 
 namespace py = pybind11;
 
 PYBIND11_MODULE( pygrit, m ) 
 {
-  py::class_<Pet>(m, "Pet")
-        .def(py::init<std::string const &>())
-        .def("set_name", &Pet::set_name)
-        .def("get_name", &Pet::get_name);
+  //using grit::param_type;
+  //using grit::engine2d_type;
+
+  using util::ConfigFile;
+
+
+  py::class_<ConfigFile>(m, "ConfigFile")
+    .def(py::init<>())
+  //.def("get_values", &ConfigFile::get_values)
+    .def("get_value" , (std::string (ConfigFile::*)( std::string const &) const) &ConfigFile::get_value)
+    .def("get_value" , (std::string (ConfigFile::*)( std::string const &
+                                                   , std::string const &) const) &ConfigFile::get_value)
+    .def("get_value" , (std::string (ConfigFile::*)( std::string const &
+                                                   , std::string const &
+                                                   , std::string const &) const) &ConfigFile::get_value)
+    .def("get_value" , (std::string (ConfigFile::*)( std::string const &
+                                                   , std::string const &
+                                                   , std::string const &
+                                                   , std::string const &) const) &ConfigFile::get_value)
+    .def("exist"     , &ConfigFile::exist)
+    .def("clear"     , &ConfigFile::clear)
+    .def("load"      , &ConfigFile::load, py::arg("filename"), py::arg("throw_on_errors") = true);
+
+  // 2018-04-23 Marek: just adding a couple of get/set functions to test if this thing works.
+  //                   Not sure if they'll be needed in the first release (as we should stick
+  //                   to reading the parameters from the cfg files), but it should be rather
+  //                   simple to add them later.
+  /*py::class_<param_type>(m, "Parameters")
+    .def(py::init([]() { return new param_type(); }) )
+    .def("get_upper_threshold", &param_type::get_upper_threshold)
+    .def("get_lower_threshold", &param_type::get_lower_threshold);*/
 }
 
