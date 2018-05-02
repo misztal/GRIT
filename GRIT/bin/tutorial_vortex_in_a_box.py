81f2b835b2ff49a808e647f68a4f540d1ae606e5
Kenny Erleben <kenny@di.ku.dk>
2018-05-02 22:46:46 +0200

    Making a nice main function in first python script

--- GRIT/bin/tutorial_vortex_in_a_box.py
+++ GRIT/bin/tutorial_vortex_in_a_box.py
@@ -1,62 +1,69 @@
-import pygrit as pg
+import pygrit as GRIT
 import math
 
-print('*** VORTEX IN A BOX ***\n')
 
-cfg_filename = '../bin/tutorial_vortex_in_a_box.cfg'
+def write_svg_files(engine, parameters, output_path, frame_number):
+    filename = output_path + GRIT.generate_filename('/tutorial_vortex_in_a_box', frame_number, 'svg')
+    GRIT.svg_draw(filename, engine, parameters)
+    print('\t\twrite_svg_files() Done writing ' + filename)
 
-settings  = pg.ConfigFile()
-settings.load(cfg_filename)
 
-parameters = pg.make_parameters_from_config_file( settings )
+def do_simulation_step(engine, settings):
+    object_label = int(settings.get_value('object_label', '1'))
+    phase = GRIT.make_phase(engine, object_label)
+    px = GRIT.VectorDouble()
+    py = GRIT.VectorDouble()
 
-txt_filename = settings.get_value('txt_filename','circle_enright.txt')
-output_path  = settings.get_value('output_path' ,''                  )
-max_steps    = int(settings.get_value('steps','100'))
+    GRIT.get_sub_range_current(engine, phase, px, py)
 
-print('Loading mesh file: '+txt_filename)
+    N = px.size()
+    dt = 0.01
+    
+    px_new = GRIT.VectorDouble(px)
+    py_new = GRIT.VectorDouble(py)
+    
+    for i in range(0, N):
+        x = px[i]
+        y = py[i]
+        u = 2.0 * math.cos(math.pi * y) * math.sin(math.pi * y) * math.sin(math.pi*x) * math.sin(math.pi*x)
+        v = -2.0 * math.cos(math.pi * x) * math.sin(math.pi * x) * math.sin(math.pi*y) * math.sin(math.pi*y)
+        px_new[i] = px[i] + u*dt
+        py_new[i] = py[i] + v*dt
 
-engine = pg.Engine2D()
+    GRIT.set_sub_range_target(engine, phase, px_new, py_new)
 
-pg.init_engine_with_mesh_file(
-                              pg.get_data_file_path(txt_filename)
+
+def main():
+    print('*** VORTEX IN A BOX ***\n')
+
+    cfg_filename = '../bin/tutorial_vortex_in_a_box.cfg'
+
+    settings = GRIT.ConfigFile()
+    settings.load(cfg_filename)
+
+    parameters = GRIT.make_parameters_from_config_file(settings)
+
+    txt_filename = settings.get_value('txt_filename', 'circle_enright.txt')
+    output_path = settings.get_value('output_path', '')
+    max_steps = int(settings.get_value('steps', '100'))
+
+    print('Loading mesh file: ' + txt_filename)
+
+    engine = GRIT.Engine2D()
+
+    GRIT.init_engine_with_mesh_file(
+                              GRIT.get_data_file_path(txt_filename)
                               , parameters
                               , engine
                               )
 
-def write_svg_files( frame_number ):
-    filename = output_path+pg.generate_filename('/tutorial_vortex_in_a_box',frame_number,'svg')
-    pg.svg_draw(filename, engine, parameters)
-    print('\t\twrite_svg_files() Done writting  '+filename)
+    write_svg_files(engine, parameters, output_path, 0)
 
-def do_simulation_step( ):
-    object_label = int(settings.get_value('object_label','1'))
-    phase        = pg.make_phase(engine, object_label)
-    px           = pg.VectorDouble()
-    py           = pg.VectorDouble()
+    for i in range(1, max_steps):
+        do_simulation_step(engine, settings)
+        engine.update(parameters)
+        write_svg_files(engine, parameters, output_path, i)
 
-    pg.get_sub_range_current(engine, phase, px, py)
 
-    N  = px.size()
-    dt = 0.01
-    
-    px_new       = pg.VectorDouble(px)
-    py_new       = pg.VectorDouble(py)
-    
-    for i in range(0,N):
-        x = px[i]
-        y = py[i]
-        u =  2.0 * math.cos( math.pi * y ) * math.sin( math.pi * y ) * math.sin( math.pi*x ) * math.sin( math.pi*x )
-        v = -2.0 * math.cos( math.pi * x ) * math.sin( math.pi * x ) * math.sin( math.pi*y ) * math.sin( math.pi*y )
-        px_new[i] = px[i] + u*dt;
-        py_new[i] = py[i] + v*dt;
-
-    pg.set_sub_range_target(engine, phase, px_new, py_new)
-
-write_svg_files(0)
-
-for i in range(1, max_steps):
-    do_simulation_step()
-    engine.update(parameters)
-    write_svg_files(i)
-
+if __name__ == "__main__":
+    main()
